098527e93e2671153987a3ed4584f998fe601f29
Mathias Vanhems <vanhems.mathias@gmail.com>
2023-03-11 20:04:07 +0100

    update

--- src/main/java/com/example/springboot/BanqueApplication.java
+++ src/main/java/com/example/springboot/BanqueApplication.java
@@ -1,4 +1,4 @@
-package com.training.springboot;
+package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
