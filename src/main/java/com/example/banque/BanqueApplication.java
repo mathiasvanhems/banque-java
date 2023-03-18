e8afb08012a06e5b1b5ea91056d25cbe32688e84
Mathias Vanhems <vanhems.mathias@gmail.com>
2023-03-18 04:56:55 +0100

    gestion des CORS

--- src/main/java/com/example/banque/BanqueApplication.java
+++ src/main/java/com/example/banque/BanqueApplication.java
@@ -2,6 +2,10 @@ package com.example.banque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class BanqueApplication {
@@ -9,5 +13,13 @@ public class BanqueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BanqueApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
+	}
 }
--- src/main/java/com/example/banque/controller/BanqueController.java
+++ src/main/java/com/example/banque/controller/BanqueController.java
@@ -4,12 +4,7 @@ import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.banque.model.Banque;
 import com.example.banque.repository.BanqueRepository;
@@ -25,6 +20,7 @@ public class BanqueController {
     }
     
     @GetMapping
+    //@CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity getAll() {
         return ResponseEntity.ok(this.banqueRepository.findAll());
     }
